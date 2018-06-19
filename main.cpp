b1072c8c72c273d3730398ed89ba5d7ceee5bb66
Rasmus Christian Pedersen <zerhacken@yahoo.com>
2018-06-19 21:50:36 +0200

    added linalg.h

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include "linalg.h"
 
 #include <vector>
 #include <random>
 
 using namespace std;
+using namespace linalg::aliases;
 
 int main(int argc, char** argv)
 {
     const size_t width = 512;
     const size_t height = 512;
 
-    vector<uint8_t> pixels(width * height * 3);
+    vector<byte3> pixels(width * height);
 
     std::random_device rd;
     std::mt19937 gen(rd()); 
@@ -19,7 +21,7 @@ int main(int argc, char** argv)
 
     for (size_t i = 0; i < pixels.size(); ++i)
     {
-        pixels[i] = dist(gen);
+        pixels[i] = byte3(dist(gen));
     }
 
     stbi_write_png("iq.png", width, height, 3, pixels.data(), width * 3);
